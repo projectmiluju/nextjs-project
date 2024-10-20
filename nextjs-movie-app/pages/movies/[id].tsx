cea824ccbf9c26bb4c11f1e58629b95b5cb9055f
projectmiluju <dnjsdyd0712@naver.com>
2024-10-20 17:11:53 +0900

    feat(exp) : docker 배포

--- nextjs-movie-app/pages/movies/[id].tsx
+++ nextjs-movie-app/pages/movies/[id].tsx
@@ -2,7 +2,12 @@ import { useRouter } from "next/router";
 import Link from "next/link";
 import styles from "../../styles/MovieDetail.module.css";
 
-const movies = {
+type Movie = {
+  title: string;
+  description: string;
+};
+
+const movies: Record<string, Movie> = {
   "1": { title: "아이언맨", description: "깡통로봇인간" },
   "2": { title: "캡틴아메리나", description: "얼음 속에서 깨어난 군인" },
   "3": { title: "토르", description: "신인척하는 외계인" },
@@ -12,9 +17,16 @@ export default function MovieDetail() {
   const router = useRouter();
   const { id } = router.query;
 
-  const movie = movies[id as string];
+  // id가 undefined일 수 있기 때문에 먼저 확인
+  if (!id || Array.isArray(id)) {
+    return <p>영화를 찾을 수 없습니다.</p>;
+  }
 
-  if (!movie) return <p>영화를 찾을 수 없습니다.</p>;
+  const movie = movies[id];
+
+  if (!movie) {
+    return <p>영화를 찾을 수 없습니다.</p>;
+  }
 
   return (
     <div className={styles.container}>
